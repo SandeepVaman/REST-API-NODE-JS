ac98f6a15eb302b433da7c6a8d9085acc45474de
SandeepVaman <sandeepvbujire@gmail.com>
2018-04-04 14:24:27 +0530

    Application is connected to the database.

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders')
+
+mongoose.connect('mongodb://admin:'+process.env.MONGO_ATLAS_PW+'@node-rest-shop-shard-00-00-zz7so.mongodb.net:27017,node-rest-shop-shard-00-01-zz7so.mongodb.net:27017,node-rest-shop-shard-00-02-zz7so.mongodb.net:27017/test?ssl=true&replicaSet=Node-Rest-Shop-shard-0&authSource=admin',
+{
+    useMongoClient: true
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
